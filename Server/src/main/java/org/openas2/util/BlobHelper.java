0957ad652ad8b6231358fa710571d3a4cfa7c1c5
azmigproject <azmigproject@outllook.com>
2018-05-30 19:52:16 +0530

    Updated changes for having blob save/download

---
+++ Server/src/main/java/org/openas2/util/BlobHelper.java
@@ -0,0 +1,64 @@
+package org.openas2.util;
+
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
+import org.openas2.lib.dbUtils.ServersSettings;
+import java.util.List;
+
+public class BlobHelper {
+
+    public boolean UploadFileInBlob(String blobContainer,String blobName,String filePath) throws Exception {
+        final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
+
+
+            AzureUtil azureUtil = new AzureUtil();
+            azureUtil.init();
+            List<ServersSettings> serverSettings = azureUtil.getServersSettings();
+            ServersSettings serverSetting = serverSettings.get(0);
+
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount =
+                    CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
+
+            // Create the queue client.
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+            // Retrieve a reference to a queue.
+            CloudBlobContainer blockBlobContainer = blobClient.getContainerReference(blobContainer);
+
+            // Create the queue if it doesn't already exist.
+            blockBlobContainer.createIfNotExists();
+            CloudBlockBlob blockBlob = blockBlobContainer.getBlockBlobReference(blobName);
+            blockBlob.uploadFromFile(filePath);
+
+        return true;
+    }
+
+
+    public boolean DownloadBlobInFile(String blobContainer,String blobName,String filePath) throws Exception {
+        final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
+
+
+        AzureUtil azureUtil = new AzureUtil();
+        azureUtil.init();
+        List<ServersSettings> serverSettings = azureUtil.getServersSettings();
+        ServersSettings serverSetting = serverSettings.get(0);
+
+        // Retrieve storage account from connection-string.
+        CloudStorageAccount storageAccount =
+                CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
+
+        // Create the queue client.
+        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+        // Retrieve a reference to a queue.
+        CloudBlobContainer blockBlobContainer = blobClient.getContainerReference(blobContainer);
+
+        // Create the queue if it doesn't already exist.
+        blockBlobContainer.createIfNotExists();
+        CloudBlockBlob blockBlob = blockBlobContainer.getBlockBlobReference(blobName);
+        blockBlob.downloadToFile(filePath);
+
+        return true;
+    }
+}
