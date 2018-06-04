e685b97dd75cace5022aafc283db512e6307043b
azmigproject <azmigproject@outlook.com>
2018-06-04 02:14:47 -0700

    Updated

--- Server/src/main/java/org/openas2/util/QueueHelper.java
+++ Server/src/main/java/org/openas2/util/QueueHelper.java
@@ -76,39 +76,43 @@ public class QueueHelper {
             List<ServersSettings> serverSettings = azureUtil.getServersSettings();
             ServersSettings serverSetting = serverSettings.get(0);
             // Retrieve storage account from connection-string.
-            CloudStorageAccount storageAccount =
-                    CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
             // Create the queue client.
             CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
             // Retrieve a reference to a queue.
             CloudQueue queue = queueClient.getQueueReference(queueName);
-            // Peek at the next message.
-            for (CloudQueueMessage message : queue.retrieveMessages(4, 60, null, null)) {
+            queue.downloadAttributes();
+            if(queue.getApproximateMessageCount() > 0)
+            {
+                for (CloudQueueMessage message : queue.retrieveMessages(4, 300, null, null)) {
 
-                String queueMessage = message.getMessageContentAsString();
-                if (queueMessage.contains("|__|")) {
-                    String[] arr = queueMessage.split("\\|\\_\\_\\|");
-                    File file = new File(outDir+"\\"+arr[0]);
-                    file.createNewFile();
-                    FileWriter writer = new FileWriter(file);
-                    writer.write(arr[1]);
-                    writer.flush();
-                    writer.close();
+                    String queueMessage = message.getMessageContentAsString();
+                    if (queueMessage.contains("|__|")) {
+                        String[] arr = queueMessage.split("\\|__\\|");
+                        File file = new File(outDir + "\\" + arr[0]);
+                        file.createNewFile();
+                        FileWriter writer = new FileWriter(file);
+                        writer.write(arr[1]);
+                        writer.flush();
+                        writer.close();
+                    }
+                    if (queueMessage.contains("|_B_|")) {
+                        String[] arr = queueMessage.split("\\|_B_\\|");
+                        //BlobHelper blob = new BlobHelper();
+                        String blobName = GetBlobName(as2Identifier, arr[0]);
+                        BlobHelper blob = new BlobHelper();
+                        //blob.UploadFileInBlob(serverSetting.getBlobContainerName(),"as10/outgoing/ship.xml","D:\\Sandeep_Work_2018\\data\\ServerFolder\\ship.xml");
+
+                        blob.DownloadBlobInFile(serverSetting.getBlobContainerName(), blobName, outDir,arr[0]);
+                    }
+
+                    // Do processing for all messages in less than 5 minutes,
+                    // deleting each message after processing.
+                    queue.deleteMessage(message);
                 }
-                if (queueMessage.contains("|_B_|")) {
-                    String[] arr = queueMessage.split("\\|\\_B_\\|");
-                    //BlobHelper blob = new BlobHelper();
-                    String blobName = GetBlobName(as2Identifier,arr[0]);
-                    BlobHelper blob = new BlobHelper();
-                    //blob.UploadFileInBlob(serverSetting.getBlobContainerName(),"as10/outgoing/ship.xml","D:\\Sandeep_Work_2018\\data\\ServerFolder\\ship.xml");
-
-                    blob.DownloadBlobInFile(serverSetting.getBlobContainerName(),blobName, outDir);
-                }
-
-                // Do processing for all messages in less than 5 minutes,
-                // deleting each message after processing.
-                queue.deleteMessage(message);
             }
+            // Peek at the next message.
+
 
         } catch (Exception e) {
             // Output the stack trace.
@@ -116,9 +120,11 @@ public class QueueHelper {
         }
         return true;
     }
-    public String GetBlobName(String as2Identifier, String fileName)
-    {
 
-        return as2Identifier+"outgoing"+fileName;
+    public String GetBlobName(String as2Identifier, String fileName) {
+
+
+        String blobName = as2Identifier+File.separator+"outgoing"+File.separator+fileName;
+        return blobName;
     }
 }
