a823d04ced1b2b1825e760bb98d1a02419f5ce31
azmigproject <azmigproject@outlook.com>
2018-06-04 02:34:58 -0700

    Updated

--- Server/src/main/java/org/openas2/util/BlobHelper.java
+++ Server/src/main/java/org/openas2/util/BlobHelper.java
@@ -3,39 +3,13 @@ package org.openas2.util;
 import com.microsoft.azure.storage.*;
 import com.microsoft.azure.storage.blob.*;
 import org.openas2.lib.dbUtils.ServersSettings;
+
+import java.io.File;
 import java.util.List;
 
 public class BlobHelper {
 
-    public boolean UploadFileInBlob(String blobContainer,String blobName,String filePath) throws Exception {
-        final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
-
-
-            AzureUtil azureUtil = new AzureUtil();
-            azureUtil.init();
-            List<ServersSettings> serverSettings = azureUtil.getServersSettings();
-            ServersSettings serverSetting = serverSettings.get(0);
-
-            // Retrieve storage account from connection-string.
-            CloudStorageAccount storageAccount =
-                    CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
-
-            // Create the queue client.
-            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-
-            // Retrieve a reference to a queue.
-            CloudBlobContainer blockBlobContainer = blobClient.getContainerReference(blobContainer);
-
-            // Create the queue if it doesn't already exist.
-            blockBlobContainer.createIfNotExists();
-            CloudBlockBlob blockBlob = blockBlobContainer.getBlockBlobReference(blobName);
-            blockBlob.uploadFromFile(filePath);
-
-        return true;
-    }
-
-
-    public boolean DownloadBlobInFile(String blobContainer,String blobName,String filePath) throws Exception {
+    public boolean UploadFileInBlob(String blobContainer,String blobName, byte[] byteContent) throws Exception {
         final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
 
 
@@ -57,7 +31,33 @@ public class BlobHelper {
         // Create the queue if it doesn't already exist.
         blockBlobContainer.createIfNotExists();
         CloudBlockBlob blockBlob = blockBlobContainer.getBlockBlobReference(blobName);
-        blockBlob.downloadToFile(filePath);
+        blockBlob.uploadFromByteArray(byteContent,100,byteContent.length);
+
+        return true;
+    }
+
+
+    public boolean DownloadBlobInFile(String blobContainer,String blobName,String filePath, String fileName) throws Exception {
+        final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
+
+
+        AzureUtil azureUtil = new AzureUtil();
+        azureUtil.init();
+        List<ServersSettings> serverSettings = azureUtil.getServersSettings();
+        ServersSettings serverSetting = serverSettings.get(0);
+
+        // Retrieve storage account from connection-string.
+        CloudStorageAccount storageAccount =
+                CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
+
+        // Create the queue client.
+        CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+        // Retrieve a reference to a queue.
+        CloudBlobContainer blockBlobContainer = blobClient.getContainerReference(blobContainer);
+        CloudBlockBlob blockBlob = blockBlobContainer.getBlockBlobReference(blobName);
+        String fileDownloadPath = filePath +File.separator+fileName;
+        blockBlob.downloadToFile(fileDownloadPath);
 
         return true;
     }
--- Server/src/main/java/org/openas2/util/QueueHelper.java
+++ Server/src/main/java/org/openas2/util/QueueHelper.java
@@ -6,32 +6,29 @@ import org.joda.time.DateTime;
 import org.openas2.lib.dbUtils.ServersSettings;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.List;
 
 public class QueueHelper {
-    public boolean AddMsgInQueue() {
-        final String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=your_storage_account;" + "AccountKey=your_storage_account_key";
+    public boolean AddMsgToQueue(String queueName, String Msg) {
 
         try {
             AzureUtil azureUtil = new AzureUtil();
             azureUtil.init();
             List<ServersSettings> serverSettings = azureUtil.getServersSettings();
             ServersSettings serverSetting = serverSettings.get(0);
-
             // Retrieve storage account from connection-string.
             CloudStorageAccount storageAccount =
                     CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
-
             // Create the queue client.
             CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
-
             // Retrieve a reference to a queue.
-            CloudQueue queue = queueClient.getQueueReference("myqueue");
-
-            // Create the queue if it doesn't already exist.
-            queue.createIfNotExists();
-            CloudQueueMessage message = new CloudQueueMessage("Hello, World");
+            CloudQueue queue = queueClient.getQueueReference(queueName);
+            // Peek at the next message.
+            CloudQueueMessage message=new CloudQueueMessage(Msg);
             queue.addMessage(message);
+
+
         } catch (Exception e) {
             // Output the stack trace.
             e.printStackTrace();
@@ -66,7 +63,7 @@ public class QueueHelper {
         return true;
     }
 
-    public boolean GetMsgFromQueue(String queueName, String outDir) {
+    public boolean GetMsgFromQueue(String queueName, String outDir, String as2Identifier) {
 
         try {
             AzureUtil azureUtil = new AzureUtil();
@@ -74,37 +71,55 @@ public class QueueHelper {
             List<ServersSettings> serverSettings = azureUtil.getServersSettings();
             ServersSettings serverSetting = serverSettings.get(0);
             // Retrieve storage account from connection-string.
-            CloudStorageAccount storageAccount =
-                    CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(serverSetting.getAzureStoragekey());
             // Create the queue client.
             CloudQueueClient queueClient = storageAccount.createCloudQueueClient();
             // Retrieve a reference to a queue.
             CloudQueue queue = queueClient.getQueueReference(queueName);
+            queue.downloadAttributes();
+            if(queue.getApproximateMessageCount() > 0)
+            {
+                for (CloudQueueMessage message : queue.retrieveMessages(4, 300, null, null)) {
+
+                    String queueMessage = message.getMessageContentAsString();
+                    if (queueMessage.contains("|__|")) {
+                        String[] arr = queueMessage.split("\\|__\\|");
+                        File file = new File(outDir + "\\" + arr[0]);
+                        file.createNewFile();
+                        FileWriter writer = new FileWriter(file);
+                        writer.write(arr[1]);
+                        writer.flush();
+                        writer.close();
+                    }
+                    if (queueMessage.contains("|_B_|")) {
+                        String[] arr = queueMessage.split("\\|_B_\\|");
+                        //BlobHelper blob = new BlobHelper();
+                        String blobName = GetBlobName(as2Identifier, arr[0]);
+                        BlobHelper blob = new BlobHelper();
+                        //blob.UploadFileInBlob(serverSetting.getBlobContainerName(),"as10/outgoing/ship.xml","D:\\Sandeep_Work_2018\\data\\ServerFolder\\ship.xml");
+
+                        blob.DownloadBlobInFile(serverSetting.getBlobContainerName(), blobName, outDir,arr[0]);
+                    }
+
+                    // Do processing for all messages in less than 5 minutes,
+                    // deleting each message after processing.
+                    queue.deleteMessage(message);
+                }
+            }
             // Peek at the next message.
 
-            for (CloudQueueMessage message : queue.retrieveMessages(20, 300, null, null)) {
-
-                String queueMessage = message.getMessageContentAsString();
-                if (queueMessage.contains("|__|")) {
-                    String[] arr = queueMessage.split("|__|");
-                    File file = new File(outDir);
-                    file.getParentFile().mkdirs();
-                    file.createNewFile();
-                }
-                if (queueMessage.contains("|_B_|")) {
-                    String[] arr = queueMessage.split("|_B_|");
-                    BlobHelper blob = new BlobHelper();
-                    blob.DownloadBlobInFile("","", outDir);
-                }
-                // Do processing for all messages in less than 5 minutes,
-                // deleting each message after processing.
-                queue.deleteMessage(message);
-            }
 
         } catch (Exception e) {
             // Output the stack trace.
             e.printStackTrace();
         }
         return true;
+    }
+
+    public String GetBlobName(String as2Identifier, String fileName) {
+
+
+        String blobName = as2Identifier+File.separator+"outgoing"+File.separator+fileName;
+        return blobName;
     }
 }
